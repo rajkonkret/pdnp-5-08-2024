43e16502839c99edc44bb500780c1bd4419e58d2
rajkonkret <rajkonkret660@gmail.com>
2024-08-05 13:56:12 +0200

    [1.6] update teksty.py

--- 1/teksty.py
+++ 1/teksty.py
@@ -5,6 +5,7 @@ print(type(tekst))
 # <class 'str'>
 
 # wszystko jest obiektem
+# teksty są niemutowalne
 """ Return a copy of the string converted to uppercase. """
 tekst.upper()
 print(tekst)  # Witaj Świecie
@@ -21,3 +22,18 @@ print(tekst_lower)
 print(tekst.lower())
 # witaj świecie
 # witaj świecie
+
+# Witaj Świecie
+# 0123456789...
+print(tekst.count("i"))  # 3
+# indeksy numerowane są od zera
+print(tekst.count("j", 0, 4))  # wynik 0 bo 0123, z prawej strony zbiór otwarty, ideks 4 nie jest brany
+
+print(tekst.removeprefix("Witaj"))  # " Świecie"
+print(tekst.removesuffix("Świecie"))  # "Witaj "
+
+tekst_zamiana = "Witaj dobry Świecie"
+print(tekst_zamiana.replace("dobry", ""))  # "Witaj  Świecie"
+print(tekst.removesuffix("Świecie").strip())  # "Witaj" strip() - usunie białe znaki np.: spacje końcowe i początkowe
+
+print(tekst[4])  # wypisanie znaku o indeksie 4 -> "j"
