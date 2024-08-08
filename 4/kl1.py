1d80056671c4139222395fc809bf4b8dab6e00b4
rajkonkret <rajkonkret660@gmail.com>
2024-08-08 15:41:55 +0200

    [4.18] add kl1

---
+++ 4/kl1.py
@@ -0,0 +1,57 @@
+# klasa - element programowania obiektowego
+# klasa - szablon, przepis
+# wskazuje minimum cech jakie ma posiadać obiekt
+# obiekt (instancja) zbudowany wg przepisu
+# definicja klasy  nie uruchamia klasy
+# dopiero budowanie obiektu uruchamia elementy w klasie
+# paradygmaty programowania obiektowego
+# hermetyzacja, dziedziczenie, polimorfizm, abstrakcja
+# pola (stan obiektu -> zmienne)
+# metody (funkcje)
+
+# Deklaracja klasy
+# PEP8 zaleca nawy kals z dużej litery
+# komentarz wielolinijkowy w kalsie (funkcji) jest traktowany jako dokumentacja
+class Human:
+    """
+    Klasa Human opisująca człowieka w pythonie
+    """
+
+    imie = ""
+    wiek = None
+    plec = "k"
+
+
+# tworzenie obiektu klasy
+cz1 = Human()
+# dokumentacja klasy
+print(Human.__doc__)
+#    Klasa Human opisująca człowieka w pythonie
+print(print.__doc__)
+#   sep
+#     string inserted between values, default a space.
+#   end
+#     string appended after the last value, default a newline.
+#   file
+#     a file-like object (stream); defaults to the current sys.stdout.
+#   flush
+#     whether to forcibly flush the stream.
+
+# pydoc -b - uruchomienie serwera z dokumentacją projektu
+# pydoc -w kl1 - generowanie pliku html z dokumentacją
+
+# sprawdźmy czy obiekt cz1 posiada pola imie, wiek, plec
+print(cz1.plec)
+print(cz1.wiek)
+print(cz1.imie)
+
+# nadanie wartości polom obiektu cz1
+cz1.imie = "Anna"
+cz1.wiek = 28
+
+print(cz1.plec)
+print(cz1.wiek)
+print(cz1.imie)
+# k
+# 28
+# Anna
