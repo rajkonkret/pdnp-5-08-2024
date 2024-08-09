eedb39b6adc944b00e425ea7baf158668bda26be
rajkonkret <rajkonkret660@gmail.com>
2024-08-09 14:58:46 +0200

    [5.13] baza.py

---
+++ 5/baza1.py
@@ -0,0 +1,20 @@
+# bazy danych - przechowują zbiory danych
+# relacyjne, nierelacyjne - sql, nosql
+# relacyjne zwykle dane w tabelach
+# nierelacyjne przechowują np.: całe obiekty (jsony)
+# postgres, mysql, mssql, mariadb, oracle
+# gadaja językiem sql
+import sqlite3  # biblioteka baza danych sqlite
+
+try:
+    conn = sqlite3.connect('baza_danych.db')
+    c = conn.cursor()
+    print("Baza zostala podłączona")
+except sqlite3.Error as e:
+    print("Bładd bazy danych", e)
+finally:
+    if conn:
+        conn.close()
+        print("Połączenie zostało zamknięte")
+# Baza zostala podłączona
+# Połączenie zostało zamknięte
